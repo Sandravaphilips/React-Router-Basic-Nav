ceef4d4de2fb975bf7bb7996c475f5e25a9c1744
Sandrava Philips <lacasandra1@gmail.com>
2019-09-16 12:55:36 +0100

    set up navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Navigation = () => {
   return (
     <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
-      </div>
+     <div className="App">
+       <h1>React Router Mini</h1>
+       <div>
+         <Link to="/home">Home</Link>
+       </div>
+       <div>
+         <Link to="/about">About</Link>
+       </div>
+       <div>
+         <Link to="/contact">Contact</Link>
+       </div>
+     </div>
     </div>
   );
 };
